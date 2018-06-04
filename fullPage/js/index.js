474fcb9baa90b65839dee0164aeb92ad9dee535f
Adlinecl <1913479231@qq.com>
2018-06-04 23:32:44 +0800

    新建文件

---
+++ fullPage/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	$("#fullpage").fullpage({
+		slidesNavigation:true,
+		continuousVertical:true,
+	});
+    // $('#fullpage').fullpage();
+    // slidesNavigation:true;
+    // continuousVertical:true;
+    // loopBottom:true,
+    // loopTop:true,
+});
